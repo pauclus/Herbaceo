60d40a5b8895ab6df20774e0f56f140da663fea2
pauclus <pauloguzman0709@gmail.com>
2025-07-29 15:16:58 -0500

    Update TipoUsoRepository.java

--- src/main/java/com/Registro/Hierbas/Infrastructure/TipoUsoRepository.java
+++ src/main/java/com/Registro/Hierbas/Infrastructure/TipoUsoRepository.java
@@ -1,5 +1,12 @@
 package com.Registro.Hierbas.Infrastructure;
 
-public interface TipoUsoRepository {
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
+import com.Registro.Hierbas.Domain.TipoUso;
+
+import reactor.core.publisher.Mono;
+
+public interface TipoUsoRepository extends ReactiveCrudRepository<TipoUso, Integer> {
+
+    Mono<TipoUso> findByNombreIgnoreCase(String nombre);
 }
